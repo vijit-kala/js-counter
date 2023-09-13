a1fd378dd8a61fd4c8a5517ef514204b3f28d255
vijit-kala <vijitkala@gmail.com>
2023-09-13 19:46:21 +0530

    Add: Some logic and CSS

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # JS Counter
 
 A simple Vanilla JS project to brush up concepts in HTML, CSS, JS.
+
+PS: Feel free to tweak it as much as you wish and ignore my bad color selections :P
---
+++ index.js
@@ -0,0 +1,34 @@
+let counter = document.querySelector('.counter');
+let decrementButton = document.querySelector('.decrement');
+let resetButton = document.querySelector('.init');
+let incrementButton = document.querySelector('.increment');
+
+// counter.addEventListener('click', () => alert('Counter clicked'));
+let counterValue = 0;
+
+decrementButton.addEventListener('click', () => {
+  if (counterValue < 1) {
+    alert('Cannot go below 0!');
+  } else {
+    counterValue -= 1;
+    counter.innerHTML = counterValue;
+    if (counterValue === 0) counter.classList.remove('positive');
+  }
+});
+
+resetButton.addEventListener('click', () => {
+  counterValue = 0;
+  counter.innerHTML = counterValue;
+  if (counterValue === 0) counter.classList.remove('positive');
+});
+
+incrementButton.addEventListener('click', () => {
+  counterValue += 1;
+  counter.innerHTML = counterValue;
+  if (counterValue > 0) counter.classList.add('positive');
+});
+
+// Sets the value to zero everytime page is loaded
+window.onload = () => {
+  counter.innerHTML = counterValue;
+};
